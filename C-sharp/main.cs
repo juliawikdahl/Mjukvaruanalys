4591988bb3c4bcf3fabc2be294c7727a18b08ca7
Julia Wikdahl <julia.wikdahl@utb.ecutbildning.se>
2022-03-30 15:04:48 +0200

    klart

--- C-sharp/main.cs
+++ C-sharp/main.cs
@@ -16,7 +16,16 @@ if animal equals "ja"
     INPUT animal_name
 
     OUTPUT "name du var alltså different_age år gammal när ditt husdjur animal_name föddes."
-ELSE 
-OUTPUT "Vad synd name!"
 
-
+else if animal equals "nej"
+OUTPUT "Vad tråkigt, gillar du djur?"
+INPUT want_one
+  if want_one equals "ja"
+  OUTPUT "Om du måste välja, vad väljer du? katt/hund?"
+  INPUT cat_dog
+     if cat_dog equals "hund"
+     OUTPUT "bra val, jag har en golden retriver"
+     else if cat_dog equals "katt"
+     OUTPUT "Super bra val! Katter är mitt favoritdjur, har tre stycken själv."
+  else if want_one equals "nej"
+  OUTPUT "Vad tråkigt name"
